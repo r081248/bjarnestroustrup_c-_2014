907324464c0615f8c5191a36b69354919a369474
Malleswara Reddy M <malleswarareddy.coding@gmail.com>
2018-03-15 12:01:18 +0530

    sample program to take input from keyboard and print the input to monitor

---
+++ take_input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(int argc,char **argv)
+{
+
+	int a;
+	cout<<"Enter integer value"<<endl;
+	cin>>a;
+	cout<<"a="<<a<<endl;
+	return 0;
+
+
+
+
+
+}
